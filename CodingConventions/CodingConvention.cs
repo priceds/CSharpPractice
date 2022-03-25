5572acf17d304d3b7baa91c8bf42fbc78b7f1d5c
Sarvesh <amsarvesh@live.com>
2022-03-25 17:27:16 +0530

    Coding Convetion File Updated

---
+++ CodingConventions/CodingConvention.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CodingConventions
+{
+    public interface IEmployee
+    {
+
+
+
+    }
+    public class Employee
+    {
+        private int EmployeeId { get; set; }
+        private string EmployeeName { get; set; }
+        public Employee()
+        {
+
+        }
+        public Employee GetDetails(int employeeName,string employeeName)
+        {
+
+
+        }
+        public double GetBonus()
+        {
+
+
+        }
+
+
+    }
+    public class CodingConvention
+    {
+        static void Main(string[] args)
+        {
+            int employeeId;
+            string employeeName;
+            bool isActive;
+
+        }
+    }
+}
